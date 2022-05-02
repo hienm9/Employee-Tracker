a37b8c98b4f7fe30232b753246c929b79bcd02b7
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-05-02 02:49:37 -0400

    update schemal.sql, still need to reference to another employee that is the manager of the current employee for the employee table

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,22 @@
-27 lines (23 sloc)  686 Bytes
-   
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
 
-USE employees;
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INTEGER (10),
+  manager_id INTEGER (10) NULL,
+  
+);
